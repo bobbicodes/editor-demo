234b44223a6e5f7a5c5f1ca51fb58cde11595704
Bobby Towers <porkostomus@mail.com>
2023-06-16 17:05:25 -0700

    init codemirror

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import { EditorState, EditorView, basicSetup } from "@codemirror/basic-setup";
+import { javascript } from "@codemirror/lang-javascript";
 
-function App() {
+export default function App() {
+  const editor = useRef();
+
+  useEffect(() => {
+    const log = (event) => console.log(event);
+    // uncomment this addEventListener and things will break
+    // document.body.addEventListener("input", log);
+
+    const state = EditorState.create({
+      doc: "a ",
+      extensions: [basicSetup, javascript()]
+    });
+    const view = new EditorView({ state, parent: editor.current });
+    return () => {
+      view.destroy();
+      document.body.removeEventListener("input", log);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        Codemirror 6 Clojure demo
+      </p>
+      <div ref={editor}></div>
     </div>
   );
 }
-
-export default App;
